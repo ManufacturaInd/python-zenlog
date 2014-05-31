73ec6d4db19c7b863e5ebe6d3d17e05d90c18e48
Ricardo Lafuente <r@manufacturaindependente.org>
2014-05-31 02:23:11 +0100

    Add install script

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='zenlog',
+      version='0.1',
+      description='Logging for the lazy',
+      author='Ricardo Lafuente',
+      author_email='r@manufacturaindependente.org',
+      url='http://github.com/manufacturaind/zenlog/',
+      download_url='https://github.com/ManufacturaInd/zenlog/tarball/master',
+      packages=['zenlog'],
+      license="GPL",
+     )
+
